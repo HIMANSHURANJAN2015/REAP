fdefbdc4fe0da5baa12560add4a46eb6d1ad3f82
HIMANSHU RANJAN <livelikehimanshu12@gmail.com>
2016-03-16 06:51:58 +0530

    Resetting master after 2nd evaluation

--- dataFetchingFiles/saveMarksComments.php
+++
@@ -1,8 +0,0 @@
-<?php
- include "connection.php";
- $input="(10004,'kumaradhara@gmail.com',3,'good1'),(10005,'kumaradhara@gmail.com',4,'good2')";
- $query = mysql_query("insert into marks_comments values '$input' ;");
- echo $query;
- echo "hello";
-?>
-
