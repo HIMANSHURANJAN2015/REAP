12340688965368197623f509f991c7ceab57e4c2
HIMANSHU RANJAN <livelikehimanshu12@gmail.com>
2016-04-13 23:34:25 +0530

    Evaluation of Challenge Scripts done git status including all the ,modifications of modal..and removed hardcoded values of email from all places..and saving it in frame2 div.....needs session variable to remove it

--- database/nextRelease.php
+++ database/nextRelease.php
@@ -2,3 +2,7 @@ ALTER TABLE `bundle_stud` DROP FOREIGN KEY `bundle_stud_ibfk_1`;
 ALTER TABLE `bundle_stud` ADD  CONSTRAINT `bundle_stud_ibfk_1` FOREIGN KEY (`bundle_id`) REFERENCES `REAP`.`bundle`(`bundle_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 //to drop constraint from bundle_Stud table
+
+ALTER TABLE `challenge`  ADD `status` ENUM('PENDING','ACCEPTED','REJECTED') NOT NULL DEFAULT 'PENDING';
+
+ALTER TABLE `challenge` CHANGE `status` `status` ENUM('PENDING','UPDATED','REJECTED') CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL DEFAULT 'PENDING';
